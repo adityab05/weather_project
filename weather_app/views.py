7b1f29dc18e5420b00483b34f75d25cdbe152499
Aditya Badve <adityabadve08@gmail.com>
2022-10-08 15:51:23 -0700

    Add documentation

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,37 +1,41 @@
 from django.shortcuts import HttpResponse, render
+from django.http import HttpResponseBadRequest
 import urllib.request
 import json
 
-from django.http import Http404, HttpResponseBadRequest
 
 
 
 def index(request):
+
     if request.method == 'POST':
+
+        # replace white space with '%20' for compatibility with API
         city = request.POST['city'].replace(" ", "%20")
         
+        # try getting weather data, throw Bad Request if exception
         try:
-            source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='
-                                        +city+'&units=metric&appid={}').read()
+            response_data = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='
+                                            +city+'&units=metric&appid={}').read()
         except Exception:
-            return HttpResponseBadRequest("No Such City Found")
+            return HttpResponseBadRequest("Bad Request")
 
 
-        list_of_data = json.loads(source)
+        response_json = json.loads(response_data)
 
-        print(list_of_data)
-
+        
         data = {
-            "country_code": str(list_of_data['name']),
-            "temp": str(list_of_data['main']['temp']) + ' °C',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
-            'main': str(list_of_data['weather'][0]['main']),
-            'description': str(list_of_data['weather'][0]['description']),
-            'icon': list_of_data['weather'][0]['icon'],
+            "country_code": str(response_json['name']),
+            "temp": str(response_json['main']['temp']) + ' °C',
+            "pressure": str(response_json['main']['pressure']),
+            "humidity": str(response_json['main']['humidity']),
+            'main': str(response_json['weather'][0]['main']),
+            'description': str(response_json['weather'][0]['description']),
+            'icon': response_json['weather'][0]['icon'],
         }
-        print(data)
+
     else:
-        data = {}
+
+        return HttpResponseBadRequest("Bad Request")
     
     return render(request, 'main/index.html', data)
